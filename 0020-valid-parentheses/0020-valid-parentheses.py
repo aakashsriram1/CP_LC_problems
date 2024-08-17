2133b2cf96c9567065374cfdc878837f9e54c6a1
Aakash Sriram <107379870+aakashsriram1@users.noreply.github.com>
2024-08-17 13:16:26 -0700

    Time: 8 ms (94.10%), Space: 11.7 MB (49.58%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,31 +1,24 @@
 class Solution(object):
     def isValid(self, s):
         stack = []
-        dictionary = { ")" : "(", "]" : "[", "}": "{" }
+        dictionary = { "}" : "{", ")" : "(", "]" : "[" }
         
         for i in s: 
             if i in dictionary:
                 if stack and stack[-1] == dictionary[i]:
                     stack.pop()
                 else:
-                    return False 
+                    return False
             else:
                 stack.append(i)
-        return True if not stack else False 
             
-            
-            
-        
-        
+        if not stack:
+            return True
+        else:
+            return False
         
         """
         :type s: str
         :rtype: bool
-        
-        
-        [ = 
-        ( =
-        { = 
-        
         """
         
