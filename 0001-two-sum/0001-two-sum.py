aeb1df1f2f077ed1a01f9f3df600b0811abbe0fc
Aakash Sriram <107379870+aakashsriram1@users.noreply.github.com>
2024-08-18 01:40:55 -0700

    Time: 35 ms (86.23%), Space: 12.5 MB (71.65%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,13 +1,15 @@
 class Solution(object):
     def twoSum(self, nums, target):
         mapped_values = {}
-
         for i in range(len(nums)): 
             complement = target - nums[i]
-            if complement in mapped_values:
-                return [mapped_values[complement], i]
+            if complement in mapped_values: 
+                return[mapped_values[complement],i]
             mapped_values[nums[i]] = i 
-
+            
+            
+            
+            
         """
         :type nums: List[int]
         :type target: int
