57c3f5994c44534e375bc8f8d2deba2e67b1f0db
Aakash Sriram <107379870+aakashsriram1@users.noreply.github.com>
2024-08-19 15:44:36 -0700

    Time: 36 ms (42.38%), Space: 12.3 MB (82.06%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -2,11 +2,10 @@ class Solution(object):
     def isPalindrome(self, s):
         ptr1 = 0 
         ptr2 = len(s) - 1
-        
         while ptr1 < ptr2: 
-            if not s[ptr1].isalnum():
-                ptr1 = ptr1 + 1 
-            elif not s[ptr2].isalnum(): 
+            if not s[ptr1].isalnum(): 
+                ptr1 = ptr1 + 1
+            elif not s[ptr2].isalnum():
                 ptr2 = ptr2 - 1 
             elif s[ptr1].lower() == s[ptr2].lower():
                 ptr1 = ptr1 + 1
@@ -14,6 +13,8 @@ class Solution(object):
             else:
                 return False
         return True
+
+
         """
         :type s: str
         :rtype: bool
