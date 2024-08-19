7adfd35e717dc7375ddc8f549e059c42ee477ac2
Aakash Sriram <107379870+aakashsriram1@users.noreply.github.com>
2024-08-19 15:30:33 -0700

    Time: 780 ms (28.68%), Space: 20.9 MB (61.12%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -4,15 +4,13 @@ class Solution(object):
         ptr2 = 1 
         profit = 0 
         while ptr2 < len(prices): 
-            if prices[ptr1] < prices[ptr2]:
+            if prices[ptr1] < prices[ptr2]: 
                 profit = max(profit, prices[ptr2] - prices[ptr1])
-            else:
-                ptr1 = ptr2
-            ptr2 = ptr2 + 1 
-                
-
+            else: 
+                ptr1 = ptr2 
+            ptr2 = ptr2 + 1
         return profit
-            
+
         """
         :type prices: List[int]
         :rtype: int
