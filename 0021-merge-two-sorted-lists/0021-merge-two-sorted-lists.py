9fd9150979846ead5b77c2ce913fcdcf26ccd416
Aakash Sriram <107379870+aakashsriram1@users.noreply.github.com>
2024-08-20 21:27:03 -0700

    Time: 26 ms (10.56%), Space: 11.8 MB (5.11%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -3,28 +3,31 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution(object):
     def mergeTwoLists(self, list1, list2):
-        dummyNode = rlist = ListNode()
+        dummy = ListNode()  # Dummy node to start the merged list
+        rlist = dummy  # Pointer to build the new list
         
-        while list1 and list2: 
-            if list1.val < list2.val: 
-                rlist.next = list1
-                list1 = list1.next 
+        while list1 and list2:  # Traverse both lists until one is exhausted
+            if list1.val >= list2.val: 
+                rlist.next = list2  # Attach the smaller node (list2)
+                list2 = list2.next  # Move to the next node in list2
             else: 
-                rlist.next = list2
-                list2 = list2.next 
-            rlist = rlist.next
-        if list1: 
-            rlist.next = list1 
-        else:
-            rlist.next = list2
-        return dummyNode.next
+                rlist.next = list1  # Attach the smaller node (list1)
+                list1 = list1.next  # Move to the next node in list1
+            rlist = rlist.next  # Move the pointer forward to the last node added
         
+        # Attach the remaining nodes, if any
+        if list1: 
+            rlist.next = list1
+        else:
+            rlist.next = list2 
+        
+        return dummy.next  # Return the merged list starting after the dummy node
         
         """
         :type list1: Optional[ListNode]
         :type list2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        
